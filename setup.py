3647712843e2922502bf65c7725d6681cbdd1510
knc6 <writetokamal.1989@gmail.com>
2026-08-06 10:14:01 -0400

    Bump version to 2026.8.6 (pure-torch unified calculator: FF + scalar/spectra/tensor property predictors)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="alignn",
-    version="2026.7.26",
+    version="2026.8.6",
     author="Kamal Choudhary, Brian DeCost",
     author_email="kamal.choudhary@nist.gov",
     description="alignn",
